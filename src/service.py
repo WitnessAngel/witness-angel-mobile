f8ad17f0633bfd4d76f91812ee1f61141974c53f
Pascal Chambon <pythoniks@gmail.com>
2019-12-08 16:43:05 +0100

    Fix OSC unix socket handling.

---
+++ src/service.py
@@ -0,0 +1,4 @@
+from waclient.background_service import main
+
+if __name__ == "__main__":
+    main()
--- src/waclient/utilities/osc.py
+++ src/waclient/utilities/osc.py
@@ -1,4 +1,6 @@
+import os
 import socket
+from pathlib import Path
 
 from kivy.logger import Logger as logger
 from kivy.utils import platform
@@ -19,9 +21,7 @@ def _get_osc_socket_options(socket_index):
         socket_options = dict(address='127.0.0.1', port=6420+socket_index, family="inet")
     else:
         socket_file = INTERNAL_APP_ROOT.joinpath(".witnessangel%d.sock" % socket_index)
-        if socket_file.exists():
-            socket_file.unlink()  # Else "Address already in use" error occurs!
-        socket_options = dict(address=str(socket_file), family="unix")
+        socket_options = dict(address=str(socket_file), port=None, family="unix")
     return socket_options
 
 
@@ -35,9 +35,12 @@ def get_osc_server(is_master=True):
     socket_name = "application" if is_master else "service"
     socket_options = _get_osc_socket_options(socket_index=socket_index)
 
-    logger.info("Binding OSC server of %s process to socket %s" % (socket_name, socket_options))
-
     def starter_callback():
+        logger.info("Binding OSC server of %s process to socket %s" % (socket_name, socket_options))
+        if socket_options["family"] == "unix":
+            socket_file = Path(socket_options["address"])
+            if socket_file.exists():
+                socket_file.unlink()  # Else "Address already in use" error occurs when listening!
         server.listen(default=True, **socket_options)
 
     return server, starter_callback
